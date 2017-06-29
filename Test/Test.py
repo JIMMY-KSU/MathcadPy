3b6850cd0b7bcc2db7b8c330ba0e7da7b6c409d7
MattWoodhead <woody.w62@gmail.com>
2017-06-29 21:23:32 +0100

    COM register test
    Testing registering new com objects

--- Test/Test.py
+++ Test/Test.py
@@ -6,9 +6,11 @@ Author: MattWoodhead
 """
 
 import win32com.client as win32
+import pythoncom
+import os
 
 def open_mathcad():
-    mcad = win32.gencache.EnsureModule("MathcadPrime.Application", 0, 1, 2)
+    print(win32.gencache.EnsureModule("MathcadPrime.Application", 0, 1, 2))
     mcad = win32.Dispatch("MathcadPrime.Application")
     mcad.Visible = True
 
@@ -23,6 +25,24 @@ def open_mathcad2():
 
 
 #open_word()
-open_mathcad()
-print("\n\n")
-open_mathcad2()
+#open_mathcad()
+#print("\n\n")
+#open_mathcad2()
+
+def register_mathcad_com():
+    methods = {}
+    try:
+        tlbpath = r"C:\Program Files\PTC\Mathcad Prime 3.1\Ptc.MathcadPrime.Automation.tlb"
+        mcad = pythoncom.LoadTypeLib(tlbpath)
+        pythoncom.RegisterTypeLib(mcad, tlbpath)
+        for i in range(mcad.GetTypeInfoCount()):
+            obj = mcad.GetDocumentation(i)[0]  # COM object name
+            CLSID = mcad.GetTypeInfo(i).GetTypeAttr().iid.__str__()  # CLSID
+            methods[obj] = CLSID  # Add object method to methods dictionary
+        return methods
+    except pythoncom.com_error:
+        print("COM error")
+        return None
+
+a = register_mathcad_com()
+print(a)
