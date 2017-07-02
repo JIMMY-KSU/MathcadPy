269738d6fc10c47d36477591d7f668044de00c6c
MattWoodhead <woody.w62@gmail.com>
2017-07-02 16:24:29 +0100

    Basic functionality
    Basic functionality now working, Adding methods.

--- MathcadPy_Comtypes.py
+++ MathcadPy_Comtypes.py
@@ -11,33 +11,17 @@ Python Win 32 extensions (https://sourceforge.net/projects/pywin32/)
 
 """
 
-import comtypes.client as CC
-import comtypes
+import os
 
-"""
-@TODO
-
-In the registry, "MathcadPrime.Application" is linked to the
-Ptc.MathcadPrime.Automation.Application part of the TLB file. Need to find a
-way to access the other top level functions that are part of
-Ptc.MathcadPrime.Automation (e.g. worksheet, inputs, outputs)
-
-Currently considering creating a new registry key, or finding a win32 or
-Comtypes method that allows stepping back a level in the com library.
-"""
-
-# First run - register
-# @TODO add something to decide if it is already registered
-#    try:
-#        tlbpath = r"C:\Program Files\PTC\Mathcad Prime 3.1\Ptc.MathcadPrime.Automation.tlb"
-#        mcad = pythoncom.LoadTypeLib(tlbpath)
-#        pythoncom.RegisterTypeLib(mcad, tlbpath)
-#    except pythoncom.com_error:
-#        print("Encountered COM error when registering Mathcad COM functions")
+try:  # Check that dependencies are importable
+    import comtypes.client as CC
+except:
+    print("The comtypes module is required")
+    quit()  # Stop script
 
 
 class Mathcad(object):
-    """ Top level Mathcad wapper class """
+    """ Mathcad application object """
     def __init__(self, visible=False):
         try:
             self.__mcadapp = CC.CreateObject("MathcadPrime.Application")
@@ -64,10 +48,9 @@ class Mathcad(object):
     def worksheets(self):
         """ lists worksheets open in the Mathcad instance """
         worksheets = []
-        for i in range(self.__mcadapp.Worksheets.Count):
-            print(self.__mcadapp.Worksheets.Item(i).Name)
-            worksheets.append(self.__mcadapp.Worksheets.Item(i).Name)
-        return worksheets
+        for i in range(self.__mcadapp.Worksheets.Count):  # no. of open sheets
+            worksheets.append(self.__mcadapp.Worksheets.Item(i).FullName)
+        return worksheets  # Returns a list of open worksheet filenames
 
     def close_all(self, save_option="Discard"):
         """ Closes all worksheets. Can specify save options before closing """
@@ -80,51 +63,84 @@ class Mathcad(object):
         else:
             print("incorrect save argument specified")
 
-#class Worksheet(object):
-#    """ Mathcad Worksheet class """
-#    def __init__(self, filepath=None):
-#        self.__mcadapp = CC.CreateObject("MathcadPrime.Application")
-#        self.__worksheet = comtypes.cast(self.__mcadapp)
-#        if filepath is not None:
-#            try:
-#                self.__mcadapp.Open(filepath)
-#            except:
-#                print("error opening file")
-#        self.name = self.__worksheet.Name
-#
-#    def activate(self):
-#        """ activates the worksheet object """
-#        self.__worksheet.Activate()
-#
-#    def IsOpen(self):
-#        """ Opens a worksheet file """
-#        return self.__worksheet.IsOpen()
-#
-#    def Close(self, save_option="Discard"):
-#        """ Closes the worksheet """
-#        if save_option in ["Discard", 2]:
-#            self.__worksheet.CloseAll(2)
-#        elif save_option in ["Prompt", 1]:
-#            self.__worksheet.CloseAll(1)
-#        elif save_option in ["Save", 0]:
-#            self.__worksheet.CloseAll(0)
-#        else:
-#            print("incorrect save argument specified")
-#
-#    def set_real_input(self, input_alias, value, units):
-#        """ Set the value of a numerical input range in the worksheet """
-#        if input_alias in self.worksheet.Inputs:
-#            self.worksheet.SetRealValue(str(input_alias), value, str(units))
+class Worksheet(object):
+    """ Mathcad Worksheet object
+
+    Either a filepath for a mathcad file can be supplied, or the
+    filepath can be set to None (or similar) and the optional
+    open_sheet_name argument can be used
+    """
+
+    def __init__(self, filepath, open_sheet_name=None):
+        self.__mcadapp = CC.CreateObject("MathcadPrime.Application")
+        self.__ws_at_init = {}
+        for i in range(self.__mcadapp.Worksheets.Count):
+            self.__ws_at_init[self.__mcadapp.Worksheets.Item(i).Name] = self.__mcadapp.Worksheets.Item(i).FullName
+        if open_sheet_name is not None:
+            for n, path in self.__ws_at_init.items():
+                if open_sheet_name == n:
+                    self.__mcadapp.Open(path)
+                    self.__obj = self.__mcadapp.ActiveWorksheet  # Fetches COM worksheet object
+                    self.Name = self.__obj.Name
+                    break
+            else:
+                print(f"open_sheet_name={open_sheet_name} does not match the name of any open worksheets")
+        if filepath is not None:
+            if os.path.isfile(filepath) and os.path.exists(filepath):
+                try:
+                    self.__mcadapp.Open(filepath)
+                    self.__obj = self.__mcadapp.ActiveWorksheet
+                except:
+                    print(f"Error opening {filepath}")
+
+    def name(self):
+        """ Returns the filename of the Worksheet object """
+        return self.__obj.Name
+
+    def inputs(self):
+        """ returns a list of the designated input fields in the worksheet """
+        _inputs = []
+        for i in range(self.__obj.Inputs.Count):  # no. of open sheets
+            _inputs.append(self.__obj.Inputs.GetAliasByIndex(i))
+        return _inputs  # Returns a list of open worksheet filenames
+
+    def outputs(self):
+        """ returns a list of the designated output fields in the worksheet """
+        outputs = []
+        for i in range(self.__obj.Outputs.Count):
+            outputs.append(self.__obj.Outputs.GetAliasByIndex(i))
+        return outputs  # Returns a list of open worksheet filenames
+
+    def activate(self):
+        """ activates the worksheet object """
+        self.__obj.Activate()
+
+    def Close(self, save_option="Save"):
+        """ Closes the worksheet """
+        if save_option in ["Discard", 2]:
+            self.__obj.Close(2)
+        elif save_option in ["Prompt", 1]:
+            self.__obj.Close(1)
+        elif save_option in ["Save", 0]:
+            self.__obj.Close(0)
+        else:
+            print("incorrect save argument specified")
+
+    def save_as(self):
+        pass
+        self.Name = self.__mcadapp.ActiveWorksheet.Name
+
+    def set_real_input(self, input_alias, value, units):
+        """ Set the value of a numerical input range in the worksheet """
+        if input_alias in self.__obj.Inputs:
+            self.__obj.SetRealValue(str(input_alias), value, str(units))
 
 
 if __name__ == "__main__":
 
-    import os
-    test = os.path.join(os.getcwd(), "Test", "test.mcdx")
-
-    print("Example usage")
-    MC = Mathcad(visible=False) # Open Mathcad with no GUI
-    print(MC.active_sheet())  # Print the name of the active sheet
+    TEST = os.path.join(os.getcwd(), "Test", "test.mcdx")
+    MC = Mathcad(visible=True) # Open Mathcad with no GUI
     MC.activate()
-    print(MC.version)
-    print(MC.worksheets())
+    WS = Worksheet(TEST)
+    print(WS.inputs())
+    print(WS.outputs())
