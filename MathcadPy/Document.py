3869d5f563ecd43ce3cb844039c13187b5e18b9c
MattWoodhead <woody.w62@gmail.com>
2017-07-28 22:32:36 +0100

    added file extraction method

--- MathcadPy/Document.py
+++ MathcadPy/Document.py
@@ -13,44 +13,77 @@ comtypes (https://github.com/enthought/comtypes)
 
 """
 
-import zipfile
+import zipfile as zf
 import pathlib
+import os
+import xml.etree.ElementTree as ET
 
 
-def _open_mcdx(filepath):
-    try:
-        iszip = zipfile.is_zipfile(filepath)
-        extension = pathlib.PurePath(filepath).suffix
-        if iszip and extension.lower() == ".mcdx":
-            print(zipfile.ZipFile(filepath).printdir())
-        else:
-            raise TypeError("This module can only open .mcdx files")
-            return False
-    except IOError:
-        print("Incorrect filepath")
-        return False
-
-
-class document(object):
+class _MathcadFile(object):
     """
     Class representing a .mcdx file.
 
     It can open and edit existing mathcad files.
     TODO - Create files from scratch
     """
-    def __init__(self, filename=None):
-        self.filename = filename
-        if filename == None:
-            self.filename == None
-        elif filename != None and not pathlib.Path(filename).is_file():
-            raise IOError("The filename does not exist\n'{}'\n".format(filename) +
+    def __init__(self, filepath=None):
+        self.filepath = filepath
+
+        self.internal_files = None
+
+        # Check to see if a filename has been passed when creating ther class
+        if filepath == None:
+            self.filepath == None
+        elif filepath != None and not pathlib.Path(filepath).is_file():
+            # Error handling for incorrect or non existent filename
+            raise IOError("The file does not exist\n'{}'\n".format(filepath) +
                           "to create a new file use document()")
-            self.filename == None
+            self.filepath == None
         else:
-            _open_mcdx(self.filename)
+            # Otherwise read in the file data to the class attributes
+            self.name = pathlib.Path(filepath).stem  # No file extension
+            self.filename = "{}.mcdx".format(self.name)
+            self._read_mcdx(self.filepath)
+            self.__zip_obj
+
+    def _read_mcdx(self, filepath):
+        """
+        Uses zipfile to extract file information from the mcdx archive
+        """
+        try:
+            iszip = zf.is_zipfile(filepath)
+            extension = pathlib.PurePath(filepath).suffix
+            if iszip and extension.lower() == ".mcdx":
+                self.__zip_obj = zf.ZipFile(filepath)
+                self.internal_files = {os.path.basename(i): i for i
+                                       in self.__zip_obj.namelist()}
+            else:
+                raise TypeError("This module can only open .mcdx files")
+                return False
+        except IOError:
+            print("Incorrect filepath")
+            return False
+
+    def _extract_file(self, filename):
+        """
+        Extracts the file with a matching filename from the archive
+        incorrect or non existent filenames rais a KeyError
+        """
+        if self.internal_files is None:
+            return False
+        else:
+            if filename in self.internal_files.keys():
+                #return self.__zip_obj.read(filename)  # Returns bytes of file
+                with self.__zip_obj.open(filename, 'r') as file:
+                    return file  # Returns bytes of file
+            else:
+                raise KeyError("No matching file exists inside the archive")
 
 
 if __name__ == "__main__":
 
     testpath = r"C:\Users\Matt\Documents\GitHub\MathcadPy\Test\Layout_test.mcdx"
-    document(testpath)
+    mcad_file = _MathcadFile(testpath)
+    ws = mcad_file.internal_files["worksheet.xml"]
+    ws_xml = mcad_file._extract_file(ws)
+    #print(ET.parse(ws_xml))
